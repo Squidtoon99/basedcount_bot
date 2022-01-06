9dcb3e372544e7d6062c983423a37b2f37ded910
Squidtoon99 <squidtoon99@gmail.com>
2022-01-05 20:15:48 -0600

    add new config needed

--- passwords.example.py
+++ passwords.example.py
@@ -1,14 +1,26 @@
 # Sensitive and Important Data
 
 class Bot:
-	def __init__(self, client_id, client_secret, user_agent, username, password, admin, mPassword):
-		self.client_id = client_id
-		self.client_secret = client_secret
-		self.user_agent = user_agent
-		self.username = username
-		self.password = password
-		self.admin = admin
-		self.mPassword = mPassword
+    def __init__(
+        self,
+        client_id,
+        client_secret,
+        user_agent,
+        username,
+        password,
+        admin,
+        mPassword,
+        subreddit="PoliticalCompassMemes",
+    ):
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.user_agent = user_agent
+        self.username = username
+        self.password = password
+        self.admin = admin
+        self.mPassword = mPassword
+        self.subreddit = subreddit
+
 
 # include bot login data here
 bot = Bot()
