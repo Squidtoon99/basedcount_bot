4cfddfaa49101235af2309aa6c1a789f53481b58
Squidtoon99 <squidtoon99@gmail.com>
2022-01-05 20:15:48 -0600

    add new config needed

--- passwords.example.py
+++ passwords.example.py
@@ -3,7 +3,15 @@
 
 class Bot:
     def __init__(
-        self, client_id, client_secret, user_agent, username, password, admin, mPassword
+        self,
+        client_id,
+        client_secret,
+        user_agent,
+        username,
+        password,
+        admin,
+        mPassword,
+        subreddit="PoliticalCompassMemes",
     ):
         self.client_id = client_id
         self.client_secret = client_secret
@@ -12,6 +20,7 @@ class Bot:
         self.password = password
         self.admin = admin
         self.mPassword = mPassword
+        self.subreddit = subreddit
 
 
 # include bot login data here
