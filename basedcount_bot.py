561781d1608aff537d2408579cc96a0c12ab3fc5
Squidtoon99 <squidtoon99@gmail.com>
2022-01-06 23:43:46 -0600

    moving commands to bot

--- basedcount_bot.py
+++ basedcount_bot.py
@@ -2,28 +2,23 @@
 # FAQ: https://reddit.com/r/basedcount_bot/comments/iwhkcg/basedcount_bot_info_and_faq/
 
 # Python Libraries
-import json
-from typing import Collection, List, Optional
-import asyncpraw as praw
-from datetime import timedelta, datetime
+from typing import List, Optional
+from datetime import datetime
 import signal
-import time
+import random
 import re
-from asyncpraw.models.helpers import SubredditHelper
-from motor.metaprogramming import Async
 from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
+from pymongo import ReturnDocument
 from asyncpraw.reddit import Comment, Reddit, Redditor, Subreddit
 from asyncpraw.exceptions import APIException
 from retrie.trie import Trie
 from retrie.retrie import Replacer
 import asyncio
-# basedcount_bot Libraries
-from commands import based, myBasedCount, basedCountUser, mostBased, removePill
 from flairs import checkFlair
 from backupDrive import backupDataBased
 import logging
 import os 
-
+import ranks
 
 # I'm going to use an environment PRODUCTION to differentiate between production and development
 if os.getenv("PRODUCTION", "0") == "1":
@@ -80,6 +75,30 @@ botName_Variations = Replacer(
     },
     match_substrings=False,  # TODO: discuss whether I should be removing substrings
 )  # I can't tell if removing substrings is a bug or a features
+
+noUserReplies = {
+    "myBased": [
+        "Hmm... I don't see you in my records, as it appears you aren't very based. I guess nobody's perfect.",
+        "[mybasedcount_clever_response_1](https://www.youtube.com/watch?v=YzKM5g_FwYU&ab_channel=TheMar%C3%ADas)",
+        "[mybasedcount_clever_response_2](https://www.youtube.com/watch?v=PNbBDrceCy8&ab_channel=TheWhoVEVO)",
+        "[mybasedcount_clever_response_3](https://i.kym-cdn.com/photos/images/original/001/535/068/29d.jpg)",
+        "[mybasedcount_clever_response_4](https://qph.fs.quoracdn.net/main-qimg-3999c004b514b9bfc6ba09527bfcd724)",
+        "[mybasedcount_clever_response_5](https://media1.giphy.com/media/Q7LP0tm86sBWIqjFCL/giphy.gif)",
+        "[mybasedcount_clever_response_6](https://www.youtube.com/watch?v=KeLtNnHEipA&ab_channel=AlltheAnime)",
+        "[mybasedcount_clever_response_7](https://www.youtube.com/watch?v=pmGNo8RL5kM&ab_channel=YeahYeahYeahsVEVO)",
+        "[mybasedcount_clever_response_8](https://www.youtube.com/watch?v=M5QGkOGZubQ&ab_channel=Fayroe)",
+    ],
+    "basedCount": [
+        "Yeah... I got nothing.",
+        "[basedcount_clever_response_1](https://www.youtube.com/watch?v=XXJ99MJ67gw&ab_channel=pepperjackcheese1)",
+        "[basedcount_clever_response_2](https://pics.me.me/never-met-him-trump-be-like-18509974.png)",
+        "[basedcount_clever_response_3](https://www.youtube.com/watch?v=ZTFfdEeB6j8&ab_channel=EnglishSingsing)",
+        "[basedcount_clever_response_4](https://media1.giphy.com/media/Q7LP0tm86sBWIqjFCL/giphy.gif)",
+        "[basedcount_clever_response_5](https://i.pinimg.com/originals/cd/c5/ff/cdc5ffaf883b13e7bcd834e464ab971a.png)",
+        "[basedcount_clever_response_6](https://www.youtube.com/watch?v=Cs57e-viIKw&ab_channel=ButtonsTheDragon)",
+        "[basedcount_clever_response_7](https://www.youtube.com/watch?v=tOlh-g2dxrI&ab_channel=e7magic)",
+    ],
+}
 
 based_list = [
         "based",
@@ -223,7 +242,7 @@ class BasedBot(Reddit):
                             if cleanContent.startswith("/removepill "):
                                 cleanContent = cleanContent.replace("/removepill ", "")
                                 user_pill_split = cleanContent.split(" ", 1)
-                                replyMessage = removePill(
+                                replyMessage = await self.removePill(
                                     user_pill_split[0], user_pill_split[1]
                                 )
                             break  # useless iteration
@@ -234,24 +253,24 @@ class BasedBot(Reddit):
 
                 for v in myBasedCount_Variations:
                     if v in content.lower():
-                        replyMessage = myBasedCount(author)
+                        replyMessage = await self.myBasedCount(author)
                         await message.reply(replyMessage)
                         break
 
                 for v in basedCountUser_Variations:
                     if v in content.lower():
-                        replyMessage = basedCountUser(content)
+                        replyMessage = await self.basedCountUser(content)
                         await message.reply(replyMessage)
                         break
 
                 for v in mostBased_Variations:
                     if v in content.lower():
-                        replyMessage = mostBased()
+                        replyMessage = await self.mostBased()
                         await message.reply(replyMessage)
                         break
 
                 if content.lower().startswith("/removepill"):
-                    replyMessage = removePill(author, content)
+                    replyMessage = await self.removePill(author, content)
                     await message.reply(replyMessage)
 
     async def readComments(self):
@@ -322,7 +341,7 @@ class BasedBot(Reddit):
                         # Calculate Based Count and build reply message
                         if not cheating:
                             if flair != "Unflaired":
-                                replyMessage = based(parentAuthor, flair, pill)
+                                replyMessage = await self.based(parentAuthor, pill)
 
                                 # Build list of users and send Cheat Report to admin
                                 await self.checkForCheating(author, parentAuthor)
@@ -341,24 +360,24 @@ class BasedBot(Reddit):
 
                 for v in myBasedCount_Variations:
                     if v in commenttext.lower():
-                        replyMessage = myBasedCount(author)
+                        replyMessage = await self.myBasedCount(author)
                         comment.reply(replyMessage)
                         break
 
                 for v in basedCountUser_Variations:
                     if commenttext.lower().startswith(v):
-                        replyMessage = basedCountUser(commenttext)
+                        replyMessage = await self.basedCountUser(commenttext)
                         comment.reply(replyMessage)
                         break
 
                 for v in mostBased_Variations:
                     if v in commenttext.lower():
-                        replyMessage = mostBased()
+                        replyMessage = await self.mostBased()
                         comment.reply(replyMessage)
                         break
 
                 if commenttext.lower().startswith("/removepill"):
-                    replyMessage = removePill(author, commenttext)
+                    replyMessage = await self.removePill(author, commenttext)
                     comment.reply(replyMessage)
                     break
 
@@ -429,6 +448,175 @@ class BasedBot(Reddit):
             await admin.message("Cheat Report", content)
 
         await self.db.basedHistory.delete_many({})
+
+
+    # User Commands 
+    async def based(self, user, pill):
+
+        # Retrieve User Data
+        count = await self.addBasedCount(user)
+        pills = await self.addPill(user, pill)
+        rank = ranks.rankName(int(count), user)
+        rankUp = ranks.rankMessage(int(count))
+
+        # Build Reply Message
+        replyMessage = ""
+        if (int(count) % 5) == 0:
+            replyMessage = (
+                "u/"
+                + user
+                + "'s Based Count has increased by 1. Their Based Count is now "
+                + str(count)
+                + ". \n\n Rank: "
+                + rank
+                + "\n\n Pills: "
+                + pills
+                + "\n\n I am a bot. Reply /info for more info."
+            )
+            if rankUp:
+                replyMessage = (
+                    "u/"
+                    + user
+                    + "'s Based Count has increased by 1. Their Based Count is now "
+                    + str(count)
+                    + ". \n\n Congratulations, u/"
+                    + user
+                    + "! You have ranked up to "
+                    + rank
+                    + "! "
+                    + rankUp
+                    + "\n\n Pills: "
+                    + pills
+                )
+        elif int(count) == 1:
+            replyMessage = (
+                "u/"
+                + user
+                + " is officially based! Their Based Count is now 1. \n\n Rank: House of Cards"
+                + "\n\n Pills: "
+                + pills
+                + "\n\n I am a bot. Reply /info for more info."
+            )
+        return replyMessage
+
+
+    async def myBasedCount(self, user):
+
+        # Retrieve User Data
+        count = await self.checkBasedCount(user)
+        pills = await self.getPills(user)
+
+        # Build Reply Message
+        if int(count) > 0:
+            rank = ranks.rankName(int(count), user)
+            replyMessage = (
+                "Your Based Count is "
+                + str(count)
+                + ". \n\n"
+                + "Rank: "
+                + rank
+                + "\n\n"
+                + "Pills: "
+                + ', '.join(pills)
+            )
+        else:
+            replyMessage = random.choice(noUserReplies["myBased"])
+        return replyMessage
+
+
+    async def basedCountUser(self, string):
+
+        # Take comment text string and remove everything except the username
+        excludedStrings = ["/u/", "u/", "basedcount_bot ", "/basedcount "]
+        for s in excludedStrings:
+            if s in string:
+                string = string.replace(s, "")
+        user = string
+
+        # Retrieve User Data
+        count = str(await self.checkBasedCount(user))
+        pills = await self.getPills(user)
+
+        # Build Reply Message
+        if int(count) > 0:
+            rank = ranks.rankName(int(count), user)
+            replyMessage = (
+                user
+                + "'s Based Count is "
+                + count
+                + ". \n\n"
+                + "Rank: "
+                + rank
+                + "\n\n"
+                + "Pills: "
+                + pills
+            )
+        else:
+            replyMessage = random.choice(noUserReplies["basedCount"])
+        return replyMessage
+
+
+    async def mostBased(self):
+        mostCountFlair = []
+        u = 1
+        async for r in self.db.dataBased.find().sort("count", -1).limit(10):
+            mostUserName = r["name"]
+            mostCount = str(r["count"])
+            mostFlair = r["flair"]
+            mostCountFlair.append(
+                str(
+                    str(u)
+                    + ". "
+                    + mostUserName
+                    + "  |  "
+                    + mostCount
+                    + "  |  "
+                    + mostFlair
+                )
+            )
+            u += 1
+
+        # Build Reply Message
+        replyMessage = (
+            "--The Top 10 Most Based Users--\n\n"
+            + "\n\n".join(mostCountFlair)
+        )
+        return replyMessage
+
+
+    # === Databased Searching and Updating ===
+
+
+    async def addBasedCount(self, user) -> int:
+        # Check if existing user and calculate based count
+        doc = await self.db.databased.find_one_and_update({"name":user}, {"$setOnInsert":{"pills":[]}, "$inc": {"count": 1}}, return_document=ReturnDocument.AFTER)
+        return doc['count']
+
+    async def checkBasedCount(self, user) -> int:
+        userProfile = await self.db.dataBased.find_one({"name": user})
+        if userProfile == None:
+            return 0
+        else:
+            return userProfile['count']
+
+    async def getPills(self, user) -> List[str]:
+
+        # Check if existing user and calculate pill list
+        userProfile = await self.db.dataBased.find_one({"name": user})
+        return (userProfile or {}).get("pills", [])
+
+
+    async def addPill(self, user, pill):
+        # Check if user exists
+        data = await self.db.dataBased.find_one_and_update({"name":user}, {"$addToSet": {"pills": pill}}, return_document=ReturnDocument.AFTER)
+        return data['pills']
+
+
+    async def removePill(self, user, pill):
+        # Parse data and get the bare string
+        data = await self.db.dataBased.find_one_and_update({"name": user}, {"$pull": {"pills": {"$eq": pill}}}, return_document=ReturnDocument.AFTER)
+        # Build Reply Message
+        return "Pill removed. Your pills: " + ", ".join(data['pills'])
 
 if __name__ == "__main__":
     from signal import signal
